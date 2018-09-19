2ee83decf2c920a05c985ab725c9261dcf63d27a
Kevin Stowe <kevincstowe@gmail.com>
2018-09-19 17:23:35 -0600

    minor updates...

--- SyntaxChecking.py
+++ SyntaxChecking.py
@@ -1,22 +1,17 @@
-"""
 print ("hello world!"
 
 scores = [1,4,5,10,6]
 
 for s in scores
     print (s)
-"""
 
-"""
 my_name = "Kevin"
 my_name.sort()
 
 print (1 + "3")
 
 1 == 1
-"""
 
-"""
 def return_person(first_name, last_name):
     age = (100)
     return {"first_name":first_name, "last_name":last_name}
@@ -24,8 +19,3 @@ def return_person(first_name, last_name):
 import json
 
 json.loads({"json": True, "json": "new json"})
-"""
-
-"""
-my_name.
-"""
